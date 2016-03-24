2f1970e912566699d1a8b3212cc4b3bdadc2460c
rshruthi91 <rshruthi91@users.noreply.github.com>
2016-03-24 09:04:55 -0400

    Changed to include branches into upto 3 points connecting. More than 3 points from a parent is still an error.

--- main.cpp
+++ main.cpp
@@ -47,6 +47,8 @@ int main(int argc, char *argv[])
   int num_branches = get_branches(parent_nodes,&branch_nodes);
   if(num_branches < 0) return EXIT_FAILURE;
 
+  qDebug() << num_branches << "branches are there in this file";
+
   return EXIT_SUCCESS;
 }
 
@@ -57,13 +59,15 @@ int get_branches( QVector<int> parentNodes,QVector<int> *branch_nodes){
       curr_val =  parentNodes[i];
       if(curr_val != -1) {
           curr_cnt = parentNodes.count(curr_val);
-          if(curr_cnt>2){
+          if(curr_cnt>4){
               qDebug() << "The SWC file is corrupt. More than 2 children for single parent";
               return -1;
             }
-          if(curr_cnt==2) {
+          if(curr_cnt>1) {
               branch_nodes->append(curr_val);
               parentNodes.replace(i,-1);
+              unsigned int j = parentNodes.indexOf(curr_val);
+              parentNodes.replace(j,-1);
               branch_count++;
             }
         }
